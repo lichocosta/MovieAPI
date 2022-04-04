9217f0e693bacd00bf26a46e96fe6f8c8f1c9c93
Lisandro Costa <lisandrocostamartorel@gmail.com>
2022-04-05 01:02:48 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+let page = 1;
+const btnPrev = document.getElementById('btnPrev');
+const btnNext = document.getElementById('btnNext');
+
+btnNext.addEventListener('click', () => {
+    if (page < 1000) {
+        page += 1;
+        loadMovies();
+    }
+});
+
+btnPrev.addEventListener('click', () => {
+    if (page > 1) {
+        page -= 1;
+        loadMovies();
+    }
+});
+
+const loadMovies = async() => {
+    try {
+        const answerAPI = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${page}`);
+    
+        console.log(answerAPI);
+        
+        //Si la respuesta es positiva
+        if (answerAPI.status === 200) {
+            const DATA_API = await answerAPI.json();
+
+            let movies = '';
+            DATA_API.results.forEach(movie => {
+                movies += 
+                `   
+                    <div class="pelicula">
+                        <div class="container-img">
+                            <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Portada de la película" class="poster">
+                        </div>
+                        <div class="container-title">
+                            <h3 class="titulo">${movie.title}</h3>
+                        </div>
+                    </div>    
+                `;
+            });
+
+            document.getElementById('contenedor').innerHTML = movies;
+
+            //Erorres
+        } else if (answerAPI.status === 401) {
+            console.log("Pusiste mal la key");
+        } else if (answerAPI.status === 404) {
+            console.log("La película no existe");
+        } else {
+            console.log("Error");
+        }
+
+    } catch(error){
+        console.log(error);
+    }
+}
+
+loadMovies();
