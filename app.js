7ff4de11db55ff4755b7d6344efb05ee0dd83ad4
Lisandro Costa <lisandrocostamartorel@gmail.com>
2022-04-06 00:21:06 +0200

    Refactorizando código

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-let page = 1;
+const MOVIE_CONTAINER = document.getElementById('contenedor');
 const btnPrev = document.getElementById('btnPrev');
 const btnNext = document.getElementById('btnNext');
 
+let page = 1;
 btnNext.addEventListener('click', () => {
-    if (page < 1000) {
-        page += 1;
-        loadMovies();
-    }
+    if (page >= 1000) return
+    page += 1;
+    loadMovies();
 });
 
 btnPrev.addEventListener('click', () => {
-    if (page > 1) {
-        page -= 1;
-        loadMovies();
-    }
+    if (page <= 1) return 
+    page -= 1;
+    loadMovies();
 });
 
 const loadMovies = async() => {
@@ -21,38 +20,36 @@ const loadMovies = async() => {
         const answerAPI = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${page}`);
     
         console.log(answerAPI);
+
+        const errorMessages = {
+            401: "Pusiste mal la key",
+            404: "La película no existe"
+        }
         
-        //Si la respuesta es positiva
-        if (answerAPI.status === 200) {
-            const DATA_API = await answerAPI.json();
-
-            let movies = '';
-            DATA_API.results.forEach(movie => {
-                movies += 
-                `   
-                    <div class="pelicula">
-                        <div class="container-img">
-                            <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Portada de la película" class="poster">
-                        </div>
-                        <div class="container-title">
-                            <h3 class="titulo">${movie.title}</h3>
-                        </div>
-                    </div>    
-                `;
-            });
-
-            document.getElementById('contenedor').innerHTML = movies;
-
-            //Erorres
-        } else if (answerAPI.status === 401) {
-            console.log("Pusiste mal la key");
-        } else if (answerAPI.status === 404) {
-            console.log("La película no existe");
-        } else {
-            console.log("Error");
+        if (answerAPI.status !== 200) {
+            return console.log(errorMessages[answerAPI.status] ?? 'Error');
         }
 
-    } catch(error){
+        const DATA_API = await answerAPI.json();
+
+        let movies = '';
+        DATA_API.results.forEach(movie => {
+            movies += 
+            `   
+                <div class="pelicula">
+                    <div class="container-img">
+                        <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Portada de la película" class="poster">
+                    </div>
+                    <div class="container-title">
+                        <h3 class="titulo">${movie.title}</h3>
+                    </div>
+                </div>    
+            `;
+        });
+
+        MOVIE_CONTAINER.innerHTML = movies;
+
+    } catch(error) {
         console.log(error);
     }
 }
